7935885b00f290dd2d519adc11df5cd5c7c272f7
Al-Amin <alaminkhanshakil@gmail.com>
2024-07-25 14:16:32 +0600

    Load order by id

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './UI-components/Home';
-import Menu, { loader as menuLoader } from './features/menu/Menu';
-import Cart from './features/cart/Cart';
-import CreateOrder from './features/order/CreateOrder';
-import Order, { loader as orderLoader } from './features/order/Order';
-import AppLayout from './UI-components/AppLayout';
-import Error from './UI-components/Error';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./UI-components/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
+import AppLayout from "./UI-components/AppLayout";
+import Error from "./UI-components/Error";
 
 const router = createBrowserRouter([
   {
@@ -13,27 +13,28 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home />,
       },
       {
-        path: '/menu',
+        path: "/menu",
         element: <Menu />,
         loader: menuLoader,
         errorElement: <Error />,
       },
       {
-        path: '/cart',
+        path: "/cart",
         element: <Cart />,
       },
       {
-        path: '/order/new',
+        path: "/order/new",
         element: <CreateOrder />,
       },
       {
-        path: '/order/:orderId',
+        path: "/order/:orderId",
         element: <Order />,
         loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -2,45 +2,13 @@ import {
   calcMinutesLeft,
   formatDate,
   formatCurrency,
-} from '../../utilities/helpers';
-import { getOrder } from '../../services/apiRestaurant';
-
-const order = {
-  id: 'ABCDEF',
-  customer: 'Jonas',
-  phone: '123456789',
-  address: 'Arroios, Lisbon , Portugal',
-  priority: true,
-  estimatedDelivery: '2027-04-25T10:00:00',
-  cart: [
-    {
-      pizzaId: 7,
-      name: 'Napoli',
-      quantity: 3,
-      unitPrice: 16,
-      totalPrice: 48,
-    },
-    {
-      pizzaId: 5,
-      name: 'Diavola',
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-    {
-      pizzaId: 3,
-      name: 'Romana',
-      quantity: 1,
-      unitPrice: 15,
-      totalPrice: 15,
-    },
-  ],
-  position: '-9.000,38.000',
-  orderPrice: 95,
-  priorityPrice: 19,
-};
+} from "../../utilities/helpers";
+import { getOrder } from "../../services/apiRestaurant";
+import { useLoaderData } from "react-router-dom";
 
 function Order() {
+  const order = useLoaderData();
+
   const {
     id,
     status,
@@ -58,11 +26,7 @@ function Order() {
         <h2>Status</h2>
         <div>
           {priority && <span>Priority</span>}
-          <span>
-            {status}
-            {' '}
-            order
-          </span>
+          <span>{status} order</span>
         </div>
       </div>
 
@@ -70,12 +34,11 @@ function Order() {
         <p>
           {deliveryIn >= 0
             ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😀`
-            : 'Order should have arrived'}
+            : "Order should have arrived"}
         </p>
         <p>
           (Estimated delivery:
-          {formatDate(estimatedDelivery)}
-          )
+          {formatDate(estimatedDelivery)})
         </p>
       </div>
 
