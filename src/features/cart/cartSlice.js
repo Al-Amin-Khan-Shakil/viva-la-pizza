8f72cd2b5c23bf39ca88df093d5e9a8faa083bcd
Al-Amin <alaminkhanshakil@gmail.com>
2024-08-23 13:24:36 +0600

    Fix linter errors-2.1

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -60,7 +60,6 @@ export const getTotalCartQuantity = (state) => (
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0)
 );
 
-
 export const getTotalCartPrice = (state) => (
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0)
 );
